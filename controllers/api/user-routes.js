d0f74f4df2248c6b3a7eef51380a4e10383f0bad
JonaThicke274 <lipatajonathan274@gmail.com>
2022-10-29 15:24:18 -0400

    completed and tested all user routes for functionality

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -46,4 +46,110 @@ router.get(`/:id`, (req, res) => {
     });
 });
 
+router.post(`/`, (req, res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        github: req.body.github
+    })
+    .then(dbUserData => {
+        req.session.save(() => {
+            // Declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ message: `Account created! You are now logged in!` });
+        })
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post(`/login`, (req, res) => {
+    User.findOne({
+        where: { email: req.body.email }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: `No user with that email address!` });
+            return;
+        }
+
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: `Incorrect password!` });
+            return;
+        }
+
+        req.session.save(() => {
+            // Declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ message: `You are now logged in!` });
+        });
+    });
+});
+
+router.post(`/logout`, (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.json({ message: `You are now logged out`}).status(204).end();
+        });
+    }
+    else {
+        console.log(`
+        ===============
+        You can't log out if you're not logged in
+        ===============`);
+        res.status(404).end();
+    }
+});
+
+router.put(`/:id`, withAuth, (req, res) => {
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        // Looks for id of user
+        if (!dbUserData[0]) {
+            res.status(404).json({ message: `No user with this id exists` });
+            return;
+        }
+        res.json({ message: `User info updated!` });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.delete(`/:id`, withAuth, (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: `No user with this id exists` });
+            return;
+        }
+        res.json({ message: `User deleted` });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
