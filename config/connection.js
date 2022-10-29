c522a24ebf6ceb72c0cafa70e322e73a2fdf1d08
JonaThicke274 <lipatajonathan274@gmail.com>
2022-10-29 14:29:55 -0400

    created GET routes for users and fixed connection.js so app/server starts properly

--- config/connection.js
+++ config/connection.js
@@ -3,18 +3,17 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-let sequelize;
 
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-}
-else {
-    // Create connection to our database, pass in your MySQL info for user name and password
-    const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-        host: `localhost`,
-        dialect: `mysql`,
-        port: 3306
-    });
-}
+// Create connection to our database, pass in your MySQL info for user name and password
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+})
+
 
 module.exports = sequelize;
